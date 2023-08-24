4d63fe463de4f548167c7fb4e06568a3d6f7f5a4
sankthomas <thomassankara74@gmail.com>
2023-08-24 23:38:38 +0300

    app routing

--- App.js
+++ App.js
@@ -1,20 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { NavigationContainer } from "@react-navigation/native";
+import Foods from "./screens/foods";
+import Food from "./screens/food";
+import Singlefood from "./screens/singlefood";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <StatusBar style="light" backgroundColor="#222222" />
+      <Stack.Navigator
+        initialRouteName="Foods"
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: "#222222",
+          },
+          headerTintColor: "#fff",
+          headerTitleAlign: "center",
+          gestureEnabled: true,
+          // gestureDirection: "vertical",
+          animation: "slide_from_bottom",
+        }}
+      >
+        <Stack.Screen
+          name="Foods"
+          component={Foods}
+          options={{
+            headerTitle: "Food Palace",
+          }}
+        />
+        <Stack.Screen
+          name="Food"
+          component={Food}
+          options={{
+            headerTitle: "",
+          }}
+        />
+        <Stack.Screen
+          name="SingleFood"
+          component={Singlefood}
+          options={{
+            headerTitle: "How To Prepare",
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
