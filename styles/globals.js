4a7de1f0ad4c561728ea952d093db388d2de7682
sankthomas <thomassankara74@gmail.com>
2023-08-24 23:38:32 +0300

    global styles

---
+++ styles/globals.js
@@ -0,0 +1,31 @@
+import { StyleSheet } from "react-native";
+
+export const globals = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+  padding: {
+    paddingHorizontal: 24,
+  },
+  heading: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "#222",
+    marginBottom: 16,
+  },
+  heading2: {
+    fontSize: 20,
+    fontWeight: "600",
+  },
+  text: {
+    fontSize: 16,
+    color: "#444",
+    lineHeight: 30,
+  },
+  image: {
+    width: "100%",
+    height: 200,
+    resizeMode: "cover",
+  },
+});
